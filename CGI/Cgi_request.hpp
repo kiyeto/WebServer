3802df69d89785f727edbee299fb3d6d3fdf105c
Brahim Amghough <bamghoug@e1r10p10.1337.ma>
2022-05-16 16:36:18 +0100

    CGI

--- CGI/Cgi_request.hpp
+++ CGI/Cgi_request.hpp
@@ -7,6 +7,10 @@
 
 class Cgi_request {
 	std::vector<std::string> meta_vars;
+
+	void	meta_vars(std::sting method){
+		meta_vars.push_back("REQUEST_METHOD=" + method);
+	}
 	public:
 		void execute(std::string &Path);
 };
